1f284256a6c236bf5367828204d279c58695fb37
Tanner Summers <tannersummers@gmail.com>
2017-09-09 15:24:58 -0700

    - Created Main activity - Created 4 supporting fragments - Created all supporting views

---
+++ src/main/java/io/tanners/taggedwallpaper/SimilarImagesFragment.java
@@ -0,0 +1,39 @@
+package io.tanners.taggedwallpaper;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SimilarImagesFragment extends Fragment {
+    private View view;
+    public static final String SIMILAR = "Similar Images";
+
+    public static SimilarImagesFragment newInstance() {
+        return new SimilarImagesFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_image_rec, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
--- src/main/java/io/tanners/taggedwallpaper/adapters/CategoryAdapter.java
+++ src/main/java/io/tanners/taggedwallpaper/adapters/CategoryAdapter.java
@@ -3,6 +3,7 @@ package io.tanners.taggedwallpaper.adapters;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,98 +22,30 @@ import java.util.ArrayList;
 import io.tanners.taggedwallpaper.R;
 import io.tanners.taggedwallpaper.data.CategoryItem;
 
-public class CategoryAdapter extends BaseAdapter {
+//public class CategoryAdapter extends BaseAdapter {
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
     private Context mContext;
     private ArrayList<CategoryItem> mItems;
     private int mLayoutId;
-    private int mBackBarLayoutId;
+//    private int mBackBarLayoutId;
 
-    public CategoryAdapter(Context mContext, ArrayList<CategoryItem> mItems, int mLayoutId, int mBackBarLayoutId) {
+//    public CategoryAdapter(Context mContext, ArrayList<CategoryItem> mItems, int mLayoutId, int mBackBarLayoutId) {
+//    public CategoryAdapter(Context mContext, ArrayList<CategoryItem> mItems, int mLayoutId, int mBackBarLayoutId) {
+    public CategoryAdapter(Context mContext, ArrayList<CategoryItem> mItems, int mLayoutId) {
         this.mContext = mContext;
         this.mItems = mItems;
         this.mLayoutId = mLayoutId;
-        this.mBackBarLayoutId = mBackBarLayoutId;
-    }
-
-    public int getCount() {
-        Log.i("POSITION", "GETCOUNT: " + mItems.size());
-        return mItems == null ? 0 : mItems.size();
-    }
-
-    public Object getItem(int position) {
-        Log.i("POSITION", "GETITEM: " + String.valueOf(position));
-
-        return null;
-    }
-
-    public long getItemId(int position) {
-        Log.i("POSITION", "GETITEMID: " + String.valueOf(position));
-        return 0;
-    }
-
-    @Override
-    public CharSequence[] getAutofillOptions() {
-        Log.i("POSITION", "getAutofillOptions: ");
-
-        return new CharSequence[0];
-    }
-
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View mItem = convertView;
-        PhotoCategoryContainerView mItemContainerView = null;
-
-        if (mItem == null) {
-            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
-            mItem = inflater.inflate(mLayoutId, parent, false);
-
-            mItemContainerView = new PhotoCategoryContainerView();
-
-            mItemContainerView.bar = mItem.findViewById(R.id.grid_image_bar);
-            mItemContainerView.image = mItem.findViewById(R.id.grid_image_background);
-            mItemContainerView.title = mItem.findViewById(R.id.grid_item_text);
-
-            mItem.setTag(mItemContainerView);
-        }
-        else
-        {
-            mItemContainerView = (PhotoCategoryContainerView) mItem.getTag();
-        }
-
-        CategoryItem mCurrentItem = mItems.get(position);
-
-//        mItemContainerView.image.setBackgroundResource(mCurrentItem.getmResourceId());
-
-        setUpImage(mCurrentItem.getmUrl(), mItemContainerView.image);
-//        setUpImage(mBackBarLayoutId, mItemContainerView.bar);
-
-//        mItemContainerView.bar.);
-//        mItemContainerView.bar.setBackgroundResource(R.color.transparentALpha80);
-//        mItemContainerView.bar.setBackgroundResource(R.color.black);
-//        mItemContainerView.bar.getBackground().setAlpha(80);
-        mItemContainerView.title.setText(mCurrentItem.getmTitle());
-
-        return mItem;
-    }
-
-    private void setUpImage(int mElement, ImageView view)
-    {
-        loadImage(Glide.with(mContext)
-                .load(mElement), view);
+//        this.mBackBarLayoutId = mBackBarLayoutId;
     }
 
     private void setUpImage(String mUrl, ImageView view)
     {
-        loadImage(Glide.with(mContext)
-                .load(mUrl), view);
-    }
-
-    private void loadImage(RequestBuilder<Drawable> mRequest, ImageView view)
-    {
         DrawableTransitionOptions transitionOptions = new DrawableTransitionOptions().crossFade();
         RequestOptions cropOptions = new RequestOptions().centerCrop();
 
-        mRequest.apply(cropOptions)
+        Glide.with(mContext)
+                .load(mUrl)
+                .apply(cropOptions)
                 //                .thumbnail()
 //                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
 //                .asBitmap()
@@ -125,11 +58,38 @@ public class CategoryAdapter extends BaseAdapter {
                 .into(view);
     }
 
-    static class PhotoCategoryContainerView
-    {
-        public TextView title;
-        public ImageView image;
-        public ImageView bar;
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(mLayoutId, parent, false);
+        return new CategoryViewHolder(view);
     }
 
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder holder, int position) {
+        CategoryItem mItem = mItems.get(position);
+        holder.title.setText(mItem.getmTitle());
+        setUpImage(mItem.getmUrl(), holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems == null ? 0 : mItems.size();
+    }
+
+    public static class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView title;
+        public ImageView image;
+
+        public CategoryViewHolder(View view) {
+            super(view);
+            this.title = (TextView) view.findViewById(R.id.row_item_text);
+            this.image = (ImageView) view.findViewById(R.id.row_image_background);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
 }
