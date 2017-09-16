4b8cec3176ea83fc8cd25c1f1890b14e4ea53f73
tanners <tannersummers@gmail.com>
2017-09-15 19:29:03 -0700

    test

--- src/main/java/io/tanners/taggedwallpaper/interfaces/IFindFragment.java
+++
@@ -1,8 +0,0 @@
-package io.tanners.taggedwallpaper.interfaces;
-
-
-import android.support.v4.app.Fragment;
-
-public interface IFindFragment {
-    public Fragment findFragmentByTitle(String title);
-}
--- src/main/java/io/tanners/taggedwallpaper/mappings/vision/request/base/BaseImage.java
+++
@@ -1,6 +0,0 @@
-package io.tanners.taggedwallpaper.mappings.vision.request.base;
-
-public abstract class BaseImage
-{
-
-}
